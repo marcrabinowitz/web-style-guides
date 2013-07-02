68a0fa620043662bea5f6ce6b57122f9670b6006
Marcus Burnette <marcus@flitehaus.com>
2013-07-01 20:59:39 -0400

    started the project with codekit, sass/compass, and a few files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8" />
+<title>Web Style Guides</title>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="js/wsg.js"></script>
+
+<link rel="stylesheet" href="css/core.css">
+
+</head>
+
+<body>
+
+<h1>Test</h1>
+<p>Paragraph</p>
+
+</body>
+</html>
