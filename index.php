2ebe39b9a605a3cc3fabac451e6d76815218f928
Marcus Burnette <marcus@flitehaus.com>
2013-07-01 21:47:46 -0400

    added some more typography styles and updated index again

--- index.php
+++ index.php
@@ -14,8 +14,21 @@
 
 <body>
 
-<h1>Web Style Guides for All!</h1>
-<p>This is where the new page will be that has lots of info on style guides for the web and then includes a template. Cool, right?</p>
+<header>
+	<h1>Web Style Guides for All!</h1>
+	<p>This is where the new page will be
+that has lots of info on style guides for the web and then includes a
+template. Cool, right?</p>
+</header>
+
+<main>
+	<h2>Heading 2</h2>
+	<h3>Heading 3</h3>
+	<h4>Heading 4</h4>
+	<h5>Heading 5</h5>
+	<h6>Heading 6</h6>
+</main>
+
 
 </body>
 </html>
