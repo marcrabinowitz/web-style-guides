0efec6d2e55ac810d3d05c2e6f7ed362b329daef
Marcus Burnette <marcus@flitehaus.com>
2013-07-01 21:20:10 -0400

    made a change to the content on the index page

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 
 <body>
 
-<h1>Test</h1>
-<p>Paragraph</p>
+<h1>Web Style Guides for All!</h1>
+<p>This is where the new page will be that has lots of info on style guides for the web and then includes a template. Cool, right?</p>
 
 </body>
 </html>
