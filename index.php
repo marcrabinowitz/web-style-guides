01665a182d06c1d6d9dc497893f99260737fa6f4
Marcus Burnette <marcus@flitehaus.com>
2013-07-06 17:49:22 -0400

    continued build out of page

--- index.php
+++ index.php
@@ -14,31 +14,65 @@
 
 <body>
 
-<header>
-	<h1>Web Style Guides for All!</h1>
-	<p>This is where the new page will be
-that has lots of info on style guides for the web and then includes a
-template. Cool, right?</p>
-</header>
+<div class="mbProject">
+<p>a <a href="http://www.mburnette.com">Marcus Burnette</a> project</p>
+</div>
 
-<main>
-	<section class="headings grid-1-3">
-		<h2 class="title">Headings</h2>
-		<h1>Heading 1</h1>
-		<h2>Heading 2</h2>
-		<h3>Heading 3</h3>
-		<h4>Heading 4</h4>
-		<h5>Heading 5</h5>
-		<h6>Heading 6</h6>
-	</section>
-	<section class="headings grid-1-3">
-		<h2 class="title">Form Elements</h2>
-		<input>
-		<h4>Heading 4</h4>
-		<h5>Heading 5</h5>
-		<h6>Heading 6</h6>
-	</section>
-</main>
+<div class="pageWrap">
+
+	<header>
+		<h1>Web Style Guides for All!</h1>
+		<div class="content">
+			<p><a href="">Style guides</a> have been used in branding for decades. They are easy for an agency or designer to set up and they ensure that design elements across the web, print, social, or environmental medias are consistent. This creates trust for potential customers and has been proven to enhance brand loyalty.</p>
+			<blockquote>Unfortunately, this process has been underutilized on the web.</blockquote>
+			<p>Sure, you're adhering to a general agreed upon look and feel when you start the project, but what happens when a client is updating their site content? What happens when they add a heading 3 and you haven't specified a style for it? Or more likely, what happens if they add a blockquote or a fieldset?</p>
+			<p>This template aims to be a checklist of sorts to ensure that all elements have been included in the CSS, whether they have been used (yet) or not. Below, you'll find a list of the items that have been included and a PSD template to get started. Over time, an HTML version will become available as well.</p>
+		</div>
+
+	</header>
+
+	<main>
+		<h2>What's Included?</h2>
+
+		<div class="headings grid-1-3">
+			<img src="http://placekitten.com/600/401" />
+			<h2>Colors & Textures</h2>
+			<p>The colors and textures of a project define how a visitor of a website will feel. It will often be the first impression made, even before a visitor sees a graphical header or a logo.</p>
+		</div>
+
+		<div class="headings grid-1-3">
+			<img src="http://placekitten.com/600/402" />
+			<h2 class="title">Typography</h2>
+			<p>Typography sets the tone. Sometimes you might need a fun, scipty typeface. Other times it needs to feel more corporate with a strong, serif typeface. And sometimes you just need Proxima Nova, right?</p>
+		</div>
+
+		<div class="headings grid-1-3">
+			<img src="http://placekitten.com/602/400" />
+			<h2 class="title">Text & Button Links</h2>
+			<p>Text</p>
+		</div>
+
+		<div class="headings grid-1-3">
+			<img src="http://placekitten.com/600/403" />
+			<h2 class="title">Form Elements</h2>
+			<p>Forms are notoriously hard to make, well, look good. Stripping the defaults away in favor of some custom styles is a good way to go.</p>
+		</div>
+
+		<div class="headings grid-1-3">
+			<img src="http://placekitten.com/601/400" />
+			<h2>Dividers</h2>
+			<p>Any time you need to separate one block or one news article listin from another, you need a divider. Sometimes this is a light hairline separator, sometimes it could be a paper tear effect.</p>
+		</div>
+
+		<div class="headings grid-1-3">
+			<img src="http://placekitten.com/603/400" />
+			<h2 class="title">Alerts/Messages</h2>
+			<p>Text</p>
+		</div>
+
+	</main>
+
+</div>
 
 </body>
 </html>
