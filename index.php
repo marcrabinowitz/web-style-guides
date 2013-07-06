283723e294cd0417c94f5d9921d5939781bf394b
Marcus Burnette <marcusdburnette@gmail.com>
2013-07-06 16:11:58 -0400

    udpated typography stuff

--- index.php
+++ index.php
@@ -14,15 +14,31 @@
 
 <body>
 
+<header>
 	<h1>Web Style Guides for All!</h1>
 	<p>This is where the new page will be
 that has lots of info on style guides for the web and then includes a
 template. Cool, right?</p>
-	<h2>Heading 2</h2>
-	<h3>Heading 3</h3>
-	<h4>Heading 4</h4>
-	<h5>Heading 5</h5>
-	<h6>Heading 6</h6>
+</header>
+
+<main>
+	<section class="headings grid-1-3">
+		<h2 class="title">Headings</h2>
+		<h1>Heading 1</h1>
+		<h2>Heading 2</h2>
+		<h3>Heading 3</h3>
+		<h4>Heading 4</h4>
+		<h5>Heading 5</h5>
+		<h6>Heading 6</h6>
+	</section>
+	<section class="headings grid-1-3">
+		<h2 class="title">Form Elements</h2>
+		<input>
+		<h4>Heading 4</h4>
+		<h5>Heading 5</h5>
+		<h6>Heading 6</h6>
+	</section>
+</main>
 
 </body>
 </html>
