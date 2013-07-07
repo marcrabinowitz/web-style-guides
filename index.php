9cf323159c72b9bd536c997613b96cd95047a5fb
Marcus Burnette <marcus@flitehaus.com>
2013-07-06 22:13:34 -0400

    completed basic layout of page/added typekit
    - completed basic page layout, accounting for varying width devices
    - added typekit with adelle and freight sans
    - kitten placeholder images
    - incomplete content, but started

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 <title>Web Style Guides</title>
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript">
+  (function() {
+    var config = {
+      kitId: 'usr0mcn',
+      scriptTimeout: 3000
+    };
+    var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+  })();
+</script>
 <script src="js/wsg.js"></script>
 
 <link rel="stylesheet" href="css/core.css">
@@ -15,15 +24,15 @@
 <body>
 
 <div class="mbProject">
-<p>a <a href="http://www.mburnette.com">Marcus Burnette</a> project</p>
+	<p>a <a href="http://www.mburnette.com">Marcus Burnette</a> project</p>
 </div>
 
 <div class="pageWrap">
 
 	<header>
-		<h1>Web Style Guides for All!</h1>
+		<h1 class="title">Web Style Guides for All!</h1>
 		<div class="content">
-			<p><a href="">Style guides</a> have been used in branding for decades. They are easy for an agency or designer to set up and they ensure that design elements across the web, print, social, or environmental medias are consistent. This creates trust for potential customers and has been proven to enhance brand loyalty.</p>
+			<p>Style guides have been used in branding for decades. They are easy for an agency or designer to set up and they ensure that design elements across the web, print, social, or environmental medias are consistent. This creates trust for potential customers and has been proven to enhance brand loyalty.</p>
 			<blockquote>Unfortunately, this process has been underutilized on the web.</blockquote>
 			<p>Sure, you're adhering to a general agreed upon look and feel when you start the project, but what happens when a client is updating their site content? What happens when they add a heading 3 and you haven't specified a style for it? Or more likely, what happens if they add a blockquote or a fieldset?</p>
 			<p>This template aims to be a checklist of sorts to ensure that all elements have been included in the CSS, whether they have been used (yet) or not. Below, you'll find a list of the items that have been included and a PSD template to get started. Over time, an HTML version will become available as well.</p>
@@ -32,42 +41,46 @@
 	</header>
 
 	<main>
-		<h2>What's Included?</h2>
+		<h2 class="included">What's Included?</h2>
 
-		<div class="headings grid-1-3">
+		<div class="kitItem">
 			<img src="http://placekitten.com/600/401" />
 			<h2>Colors & Textures</h2>
 			<p>The colors and textures of a project define how a visitor of a website will feel. It will often be the first impression made, even before a visitor sees a graphical header or a logo.</p>
 		</div>
 
-		<div class="headings grid-1-3">
+		<div class="kitItem">
 			<img src="http://placekitten.com/600/402" />
 			<h2 class="title">Typography</h2>
 			<p>Typography sets the tone. Sometimes you might need a fun, scipty typeface. Other times it needs to feel more corporate with a strong, serif typeface. And sometimes you just need Proxima Nova, right?</p>
 		</div>
 
-		<div class="headings grid-1-3">
+		<div class="kitItem">
 			<img src="http://placekitten.com/602/400" />
 			<h2 class="title">Text & Button Links</h2>
 			<p>Text</p>
 		</div>
 
-		<div class="headings grid-1-3">
+		<div class="kitItem">
 			<img src="http://placekitten.com/600/403" />
 			<h2 class="title">Form Elements</h2>
 			<p>Forms are notoriously hard to make, well, look good. Stripping the defaults away in favor of some custom styles is a good way to go.</p>
 		</div>
 
-		<div class="headings grid-1-3">
+		<div class="kitItem">
 			<img src="http://placekitten.com/601/400" />
 			<h2>Dividers</h2>
 			<p>Any time you need to separate one block or one news article listin from another, you need a divider. Sometimes this is a light hairline separator, sometimes it could be a paper tear effect.</p>
 		</div>
 
-		<div class="headings grid-1-3">
-			<img src="http://placekitten.com/603/400" />
+		<div class="kitItem">
+			<img src="http://placekitten.com/604/400" />
 			<h2 class="title">Alerts/Messages</h2>
 			<p>Text</p>
+		</div>
+
+		<div class="downloadPSD">
+			<a href="#">Download PSD &darr;</a>
 		</div>
 
 	</main>
